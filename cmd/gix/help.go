6cdc142fe8c1762c72a527081bb9b4b6562543b9
Kosarev Vsevolod <iamvkosarev@gmail.com>
2025-09-16 21:50:37 +0500

    Modify help command

--- cmd/gix/help.go
+++ cmd/gix/help.go
@@ -9,20 +9,37 @@ func help() {
 	cmdColor := color.New(color.FgHiMagenta)
 	cmdInTextColor := color.New(color.FgCyan)
 	argColor := color.New(color.FgYellow)
-	printCmd := cmdColor.PrintfFunc()
+	formatCmd := cmdColor.SprintFunc()
 	formatCmdInText := cmdInTextColor.SprintFunc()
 	formatArg := argColor.SprintFunc()
 
-	color.New(color.BgHiMagenta).Printf("gix - like X-Men but about Git")
+	color.New(color.BgHiMagenta).Printf("gix - a bit powered version of Git")
 	fmt.Printf("\n\n")
 	color.Unset()
-	printCmd("back")
+	printBackCommandHelp(formatCmd, formatCmdInText)
+
+	printCommitCommandHelp(formatCmd, formatCmdInText)
+	printCommitCommandArgsHelp(formatArg)
+
+	printSaveCommandHelp(formatCmd, formatCmdInText)
+	printCommitCommandArgsHelp(formatArg)
+}
+
+func printBackCommandHelp(formatCmd func(a ...interface{}) string, formatCmdInText func(a ...interface{}) string) {
 	fmt.Printf(
-		" - reset the commit and print the command to undo it. Based on %s command.\n\n",
+		"%s - reset last commit and print the command to undo it.\nBased on %s command.\n\n", formatCmd("back"),
 		formatCmdInText("git reset --soft HEAD~1"),
 	)
-	printCmd("commit")
-	fmt.Printf(" - classic %s command, but with addition flags to modify time:\n", formatCmdInText("git commit"))
+}
+
+func printCommitCommandHelp(formatCmd func(a ...interface{}) string, formatCmdInText func(a ...interface{}) string) {
+	fmt.Printf(
+		"%s - classic %s command, but with addition flags to modify time:\n", formatCmd("commit"),
+		formatCmdInText("git commit"),
+	)
+}
+
+func printCommitCommandArgsHelp(formatArg func(a ...interface{}) string) {
 	argsDiscMap := map[string]string{
 		"pD": "plus days",
 		"pH": "plus hours",
@@ -46,4 +63,14 @@ func help() {
 	for _, arg := range order {
 		fmt.Printf("  -%s - %s\n", formatArg(arg), argsDiscMap[arg])
 	}
+	fmt.Printf("\n")
+}
+
+func printSaveCommandHelp(formatCmd func(a ...interface{}) string, formatCmdInText func(a ...interface{}) string) {
+	fmt.Printf(
+		"%s - same %s command, but with additional saving step of all changes.\nBased on %s command.\n",
+		formatCmd("save"),
+		formatCmd("commit"),
+		formatCmdInText("git add ."),
+	)
 }
